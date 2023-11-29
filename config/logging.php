d48ff8753dc925ca2a0ca6353bea987bfd64652b
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-11-29 14:49:23 +0100

    Test

--- config/logging.php
+++ config/logging.php
@@ -19,6 +19,16 @@ return [
     */
 
     'default' => env('LOG_CHANNEL', 'stack'),
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+        ],
+        'single' => [
+            'driver' => 'errorlog',
+            'level' => 'debug',
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: "public/build",
+    },
 });
