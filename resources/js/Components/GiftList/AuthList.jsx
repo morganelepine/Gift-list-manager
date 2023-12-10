a9c1fa1e8abf504abc5d1154917bbe0b8b1090a2
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-12-10 21:06:22 +0100

    Empty list

--- resources/js/Components/GiftList/AuthList.jsx
+++ resources/js/Components/GiftList/AuthList.jsx
@@ -5,7 +5,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ListOfIdeas from "@/Components/GiftList/Ideas/All/ListOfIdeas";
 
 export default function AuthList({ auth, list, ideas }) {
-    console.log("list : ", list);
+    console.log("ideas : ", ideas);
 
     return (
         <AuthenticatedLayout
@@ -15,7 +15,7 @@ export default function AuthList({ auth, list, ideas }) {
                     <h2 className="font-semibold text-xl text-gray-800 leading-tight">
                         Ma liste "{list.name}"
                     </h2>
-                    {list && (
+                    {ideas.length > 0 && (
                         <Link
                             as="button"
                             href={route("ideas.create_idea", list.id)}
@@ -41,7 +41,7 @@ export default function AuthList({ auth, list, ideas }) {
             }
         >
             <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-4">
-                {list ? (
+                {ideas.length > 0 ? (
                     <div className="flex w-full">
                         <ListOfIdeas
                             key={list.id}
