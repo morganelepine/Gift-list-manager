4e5f0e5f4a13172c5b3b23d724026e4a49e92763
morganelepine <114992081+morganelepine@users.noreply.github.com>
2024-01-28 18:43:09 +0100

    Update list name

--- resources/js/Pages/GiftList/FirstAuthList.jsx
+++ resources/js/Pages/GiftList/FirstAuthList.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ListOfIdeas from "@/Components/GiftList/Ideas/All/ListOfIdeas";
+import EditListTitle from "@/Components/GiftList/Lists/EditListTitle";
 
 //-----------DISPLAY THIS PAGE WHEN THE AUTH USER ONLY HAVE ONE LIST IN DB
 
 export default function AuthList({ auth, list, ideas }) {
     // console.log("list[0].id : ", list[0].id);
 
+    const [editing, setEditing] = useState(false);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <div className="sm:flex items-center justify-between">
-                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                        Ma liste "{list[0].name}"
-                    </h2>
+                    <div className="flex items-center">
+                        {editing ? (
+                            <EditListTitle
+                                list={list}
+                                setEditing={setEditing}
+                            />
+                        ) : (
+                            <>
+                                <h2 className="font-semibold text-xl text-gray-800 leading-tight mr-3">
+                                    Ma liste "{list[0].name}"
+                                </h2>
+                                <button onClick={() => setEditing(true)}>
+                                    <svg
+                                        xmlns="https://www.w3.org/2000/svg"
+                                        className="h-5 w-5 text-gray-400 hover:text-indigo-800"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                </button>
+                            </>
+                        )}
+                    </div>
                     {ideas.length > 0 && (
                         <div className="flex flex-wrap mt-2 sm:mt-0">
                             <Link
