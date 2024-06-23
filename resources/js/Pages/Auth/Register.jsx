5b845842b09f71144057ad48504ae165e76e46ed
morganelepine <114992081+morganelepine@users.noreply.github.com>
2024-06-23 12:52:13 +0200

    Add reCAPTCHA

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -132,6 +132,7 @@ export default function Register() {
 
                 <div className="mt-4">
                     <ReCAPTCHA
+                        // sitekey={"6LeVO_4pAAAAABdABWA0tJvWKyUeGk-X9j3tMeqt"}
                         sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
                         onChange={handleRecaptcha}
                     />
